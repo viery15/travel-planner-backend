890a8d40f167e7c53935d260930275abce5b8d63
viery <vierydarmawan2@gmail.com>
2020-05-26 20:04:04 +0700

    fungsi get status buka tutup selesai

--- Controller/MainFeature.js
+++ Controller/MainFeature.js
@@ -28,7 +28,9 @@ exports.index = async function (req, res) {
     userLocation,
     tanggalWisata
   );
-  var itinerary = await setPriority(tujuanWisata);
+  tujuanWisata = await setSentiment(tujuanWisata);
+
+  // var itinerary = await setItinerary(tujuanWisata[0], userLocation);
 
   response.ok(tujuanWisata, res);
 };
@@ -36,23 +38,17 @@ exports.index = async function (req, res) {
 async function getTujuanWisata(kategori, userLocation, tanggalWisata) {
   var tujuan = await getByKategori(kategori);
   tujuan = await eliminasiJarak(tujuan, userLocation);
-  tujuan = await bagiKategori(tujuan);
   tujuan = await bagiHari(tujuan, tanggalWisata);
 
   return tujuan;
 }
 
-async function setPriority(tujuan) {
+async function setSentiment(tujuan) {
   for (let index = 0; index < tujuan.length; index++) {
-    for (var key in tujuan[index]) {
-      for (let i = 0; i < tujuan[index][key].length; i++) {
-        var maxScore = 0;
-        if (tujuan[index][key][i].sentiment_score > maxScore) {
-          maxScore = tujuan[index][key][i].sentiment_score
-        }
-      }
-    }
+    tujuan[index] = tujuan[index].sort(compare);
   }
+
+  return tujuan;
 }
 
 function getByKategori(kategori) {
@@ -92,6 +88,8 @@ async function eliminasiJarak(tujuan, userLocation) {
     }
   }
 
+  // console.log(dataJarak.rows[0].elements[0]);
+
   return tujuanBaru;
 }
 
@@ -121,54 +119,21 @@ async function convertMilKilo(mil) {
   return kilo;
 }
 
-async function bagiKategori(tujuan) {
-  var rangeJarak = {
-    dekat: [0, 10],
-    menengah: [11, 20],
-    jauh: [21, 100],
-  };
-
-  var tujuanBaru = {
-    dekat: [],
-    menengah: [],
-    jauh: [],
-  };
-
-  for (let index = 0; index < tujuan.length; index++) {
-    for (var key in rangeJarak) {
-      if (
-        tujuan[index].jarak >= rangeJarak[key][0] &&
-        tujuan[index].jarak <= rangeJarak[key][1]
-      ) {
-        tujuanBaru[key].push(tujuan[index]);
-      }
-    }
-  }
-
-  return tujuanBaru;
-}
-
 async function bagiHari(tujuan, tanggalWisata) {
   var jumlahHari = await getJumlahHari(tanggalWisata);
   var i = 0;
   var tujuanBaru = [];
 
-  for (var key in tujuan) {
-    for (let index = 0; index < tujuan[key].length; index++) {
-      if (tujuanBaru[i] == null) {
-        tujuanBaru[i] = {
-          dekat: [],
-          menengah: [],
-          jauh: [],
-        };
-      }
+  for (let index = 0; index < tujuan.length; index++) {
+    if (tujuanBaru[i] == null) {
+      tujuanBaru[i] = [];
+    }
 
-      tujuanBaru[i][key].push(tujuan[key][index]);
-      if (i == jumlahHari - 1) {
-        i = 0;
-      } else {
-        i++;
-      }
+    tujuanBaru[i].push(tujuan[index]);
+    if (i == jumlahHari - 1) {
+      i = 0;
+    } else {
+      i++;
     }
   }
 
@@ -182,4 +147,149 @@ function getJumlahHari(tanggalWisata) {
   let differentDays = Math.ceil(timeDifference / (1000 * 3600 * 24));
 
   return differentDays + 1;
+}
+
+function compare(a, b) {
+  if (a.sentiment_score < b.sentiment_score) {
+    return 1;
+  }
+  if (a.sentiment_score > b.sentiment_score) {
+    return -1;
+  }
+  return 0;
+}
+
+async function setItinerary(tujuan, start) {
+  var jamMulai = "07:00";
+
+  for (let index = 0; index < tujuan.length; index++) {
+    var durasi = await getJarak(start, tujuan[index].location);
+    var jamSelesai = await hitungJam(
+      jamMulai,
+      durasi.rows[0].elements[0].duration.text
+    );
+    console.log(jamSelesai);
+  }
+}
+
+function hitungJam(jam, durasi) {
+  jam = jam.split(":");
+  durasi = durasi.split(" ");
+
+  var hasil = {};
+  if (durasi.length > 2) {
+    var total_menit = parseInt(durasi[0]) * 60 + parseInt(durasi[2]);
+  } else if (durasi.length == 2) {
+    if (durasi[1] == "mins") {
+      var total_menit = parseInt(durasi[0]);
+    } else {
+      var total_menit = parseInt(durasi[0]) * 60;
+    }
+  } else if (durasi.length < 2) {
+    var total_menit = parseInt(durasi[0]) * 60;
+  }
+
+  if (total_menit > 60) {
+    var durasi_menit = total_menit % 60;
+    var durasi_jam = (total_menit - durasi_menit) / 60;
+
+    hasil.jam = parseInt(jam[0]) + durasi_jam;
+    hasil.menit = parseInt(jam[1]) + durasi_menit;
+
+    if (hasil.menit > 60) {
+      hasil.jam = (hasil.menit - (hasil.menit % 60)) / 60 + hasil.jam;
+      hasil.menit = hasil.menit - 60;
+    }
+
+    if (hasil.jam < 10) {
+      hasil.jam = "0" + hasil.jam;
+    }
+
+    if (hasil.menit < 10) {
+      hasil.menit = "0" + hasil.menit;
+    }
+
+    hasil.text = hasil.jam + ":" + hasil.menit;
+  } else if (total_menit < 60) {
+    hasil.jam = parseInt(jam[0]);
+    hasil.menit = parseInt(jam[1]) + total_menit;
+
+    if (hasil.menit > 60) {
+      hasil.jam = (hasil.menit - (hasil.menit % 60)) / 60 + hasil.jam;
+      hasil.menit = hasil.menit - 60;
+    }
+
+    if (hasil.jam < 10) {
+      hasil.jam = "0" + hasil.jam;
+    }
+
+    if (hasil.menit < 10) {
+      hasil.menit = "0" + hasil.menit;
+    }
+
+    hasil.text = hasil.jam + ":" + hasil.menit;
+  } else if (total_menit == 60) {
+    hasil.jam = parseInt(jam[0]);
+    hasil.menit = parseInt(jam[1]);
+
+    hasil.jam = hasil.jam + 1;
+
+    if (hasil.jam < 10) {
+      hasil.jam = "0" + hasil.jam;
+    }
+
+    if (hasil.menit < 10) {
+      hasil.menit = "0" + hasil.menit;
+    }
+
+    hasil.text = hasil.jam + ":" + hasil.menit;
+  }
+
+  return hasil.text;
+}
+
+function getStatus(data, waktuBerkunjung, tanggalBerkunjung) {
+  //format waktu berkunjung --> "15:00 - 18:30"
+
+  var d = new Date(tanggalBerkunjung);
+  var day = d.getDay();
+
+  var days = {
+    Senin: 1,
+    Selasa: 2,
+    Rabu: 3,
+    Kamis: 4,
+    Jumat: 5,
+    Sabtu: 6,
+    Minggu: 0,
+  };
+
+  for (var key in days) {
+    if (days[key] == day) {
+      day = key;
+    }
+  }
+
+  for (var hari in data) {
+    if (hari == day) {
+      var jam_buka = data[hari];
+    }
+  }
+
+  jam_buka = jam_buka.split("–");
+  jam_buka[0] = parseInt(jam_buka[0]);
+  jam_buka[1] = parseInt(jam_buka[1]);
+
+  waktuBerkunjung = waktuBerkunjung.split(" - ");
+  waktuBerkunjung[0] = parseInt(waktuBerkunjung[0]);
+  waktuBerkunjung[1] = parseInt(waktuBerkunjung[1]);
+  if (waktuBerkunjung[1] >= jam_buka[1] || waktuBerkunjung[0] < jam_buka[0]) {
+    var status = "Tutup";
+  } else if (
+    waktuBerkunjung[1] < jam_buka[1] ||
+    waktuBerkunjung[0] >= jam_buka[0]
+  ) {
+    var status = "Buka";
+  }
+  return status;
 }
